ec23d414daa8016334c76a0738d4e1bca5e165b6
canvor <lightimehpq@gmail.com>
2018-04-20 15:40:39 +0800

    加入 loading 模块

---
+++ src/components/Loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Spin fix>
+        <Icon type="load-c" size=18 class="demo-spin-icon-load"></Icon>
+        <div>Loading...</div>
+    </Spin>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  data () {
+    return {
+      //msg: '正在加载'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .demo-spin-icon-load{
+    animation: ani-demo-spin 1s linear infinite;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from '@/components/Layout'
+import Loading from '@/components/Loading'
 
 Vue.use(Router)
 
@@ -10,7 +11,12 @@ export default new Router({
       path: '/read/:path*',
       name: 'Layout',
       component: Layout,
-    }
+    },
+    {
+      path: '/loading',
+      name: "Loading",
+      component: Loading,
+    },
   ],
   scrollBehavior (to, from, savedPosition) {
     // return 期望滚动到哪个的位置, 如果之前滚动条不在起始位置，则有 savedPosition
