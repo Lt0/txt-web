86f340ea17e908fc6d73a8ff09e5c4aaebb12e25
canvor <lightimehpq@gmail.com>
2018-04-23 10:37:45 +0800

    添加 SetSlideItem 组件的使用说明

--- src/components/common/SetSlideItem.vue
+++ src/components/common/SetSlideItem.vue
@@ -3,7 +3,6 @@
         <div v-html="title"></div>
         <div class="slider app-btn-gutter-l">
             <Slider :value="value" @input="changeSlide" show-input :min="min" :max="max" @on-change="changeSlide"></Slider>
-            <!-- <Slider :something="vmVal" @input="changeSlide" show-input :min="min" :max="max" @on-change="changeSlide"></Slider> -->
         </div>
         <Tooltip placement="top" :content="btnTips">
             <Button type="ghost" class="app-btn-gutter-l" @click="clickBtn">
@@ -14,12 +13,20 @@
 </template>
 
 <script>
+// 使用说明
+// 示例：<SetSlideItem title='文字间隔' v-model="theme.letterSpacing" :min="-2" :max="20" @changeSlide="setEvHandler" btnTips="应用的默认值" @clickBtn="theme.letterSpacing=defaultTheme.letterSpacing; setEvHandler();" />
+// [:]title: 设置项最左边显示的名字
+// v-model: 双向绑定的变量
+// :min: 滑块的最小值
+// :max: 滑块的最大值
+// @changeSlide: 滑块操作结束后触发的事件
+
+// btnTips: 默认按钮的 Tooltips 说明
+// @clickBtn: 点击默认按钮触发的事件
+
+
 export default {
-    //title: 设置项最左边显示的名字
-    //value: 滑块的值， v-model 所配置的值会以 value 的变量名传递进来当前组件
-    //min: 滑块的最小值
-    //max: 滑块的最大值
-    //btnTips: 默认按钮的 Tooltips 说明
+    //[:]value: 滑块的值， v-model 所配置的值会以 value 的变量名传递进来当前组件
     props: ['title', 'value', 'min', 'max', 'btnTips'],
 
     methods: {
@@ -36,6 +43,7 @@ export default {
 <style scoped>
     .set-item{
         display: flex;
+
         align-items: center;
         justify-content: center;
     }
